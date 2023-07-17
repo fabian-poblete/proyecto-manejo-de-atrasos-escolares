d16acbc2994aa9e7906451c42388941f35dca7a4
fabian-poblete <fabian.poblete@cftsa.cl>
2023-07-17 12:15:42 -0400

    aplicacion funcional con mejoras aplicables

--- manejoAtrasos.py
+++ manejoAtrasos.py
@@ -13,7 +13,8 @@ db_password = os.environ.get('DB_PASSWORD')
 db_name = os.environ.get('DB_NAME')
 
 try:
-    # Conectarse a la nueva base de datos utilizando los nuevos valores de las variables de entorno
+    # Conectarse a la nueva base de datos utilizando los nuevos valores de las
+    # variables de entorno
     conexion = mysql.connector.connect(
         host=db_host,
         user=db_user,
@@ -23,7 +24,51 @@ try:
     cursor = conexion.cursor()
 
     def mostrar_datos(nombre, curso, hora_actual):
-        messagebox.showinfo("Datos del estudiante", f"Nombre: {nombre}\nCurso: {curso}\nHora actual: {hora_actual}")
+        # Obtener la fecha actual
+        fecha_actual = datetime.datetime.now()
+
+        # Obtener el número del mes con cero antecedente si es necesario
+        numero_mes = str(fecha_actual.month).zfill(2)
+
+        # Configurar la fuente Arial
+        fuente_arial = ("Arial", 12)
+
+        # Crear una ventana personalizada
+        ventana_personalizada = tk.Toplevel()
+        ventana_personalizada.title("Datos del estudiante")
+        ventana_personalizada.geometry("400x300")
+
+        # Configurar la fuente de la ventana personalizada
+        ventana_personalizada.option_add("*Font", fuente_arial)
+
+        # Crear el mensaje con el formato deseado
+        mensaje = f"Fundación Chaminade\nInstituto Linares\n\n"
+        mensaje += f"{nombre}\n\n"
+        mensaje += f"AUTORIZACIÓN\n\n"
+        mensaje += f"Curso: {curso}\n\n"
+        mensaje += f"Ingresar a clase a las: {hora_actual}\n"
+        mensaje += f"Retirarse de Clases a las: {hora_actual}\n\n"
+        mensaje += f"Firma y Timbre\nLinares: {fecha_actual.day} de {numero_mes} de {fecha_actual.year}"
+
+        # Mostrar el mensaje en la ventana personalizada
+        etiqueta_mensaje = tk.Label(ventana_personalizada, text=mensaje)
+        etiqueta_mensaje.grid(row=0, column=0, padx=10, pady=10)
+
+        # Cargar el logo
+        ruta_logo = "C:/Users/usuario/Desktop/Proyectos/Instituto-Linares/Programa-Instituto/logo.jfif"  # Reemplaza con la ruta correcta de tu logo
+        logo = Image.open(ruta_logo)
+        logo = logo.resize((50, 50))
+        logo = ImageTk.PhotoImage(logo)
+
+        # Mostrar el logo en la esquina derecha
+        contenedor_logo = tk.Frame(ventana_personalizada)
+        contenedor_logo.grid(row=0, column=1, padx=0.5, pady=0.5, sticky="ne")  # sticky="ne" para alinear en la esquina derecha
+
+        etiqueta_logo = tk.Label(contenedor_logo, image=logo)
+        etiqueta_logo.pack(padx=10, pady=20)  # Ajusta el valor de padx y pady para aumentar la separación
+
+        # Ejecutar el bucle de eventos de la ventana personalizada
+        ventana_personalizada.mainloop()
 
     def obtener_datos_estudiante(rut):
         consulta = "SELECT rut, nombre_completo, curso FROM estudiantes WHERE rut = %s"
@@ -32,24 +77,28 @@ try:
 
         if resultado:
             rut, nombre, curso = resultado
-            hora_actual = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            hora_actual = datetime.datetime.now().strftime("%H:%M:%S")
             mostrar_datos(nombre, curso, hora_actual)
         else:
-            messagebox.showerror("Error", "No se encontró ningún estudiante con el RUT ingresado.")
+            messagebox.showerror(
+                "Error", "No se encontró ningún estudiante con el RUT ingresado.")
 
     def obtener_datos():
         rut = rut_entry.get()
         obtener_datos_estudiante(rut)
 
+    # Configurar la fuente Arial
+    fuente_arial = ("Arial", 12)
+
     # Crear la interfaz gráfica
     ventana = tk.Tk()
-    ventana.title("Consulta de datos de estudiante")
+    ventana.title("Autorización")
     ancho_pantalla = ventana.winfo_screenwidth()
     alto_pantalla = ventana.winfo_screenheight()
 
     # Establecer las dimensiones de la ventana
-    ancho_ventana = 400
-    alto_ventana = 400
+    ancho_ventana = 300
+    alto_ventana = 300
 
     # Calcular la posición x, y para centrar la ventana
     posicion_x = int((ancho_pantalla / 2) - (ancho_ventana / 2))
@@ -59,23 +108,52 @@ try:
     ventana.geometry(f"{ancho_ventana}x{alto_ventana}+{posicion_x}+{posicion_y}")
 
     ventana.resizable(False, False)
-    ruta_logo = "C:/Users/usuario/Desktop/pdf/respaldo/logo.jfif"  # Reemplaza con la ruta de tu imagen de logo
+    # Reemplaza con la ruta de tu imagen de logo
+    ruta_logo = "C:/Users/usuario/Desktop/Proyectos/Instituto-Linares/Programa-Instituto/logo.jfif"
     logo = Image.open(ruta_logo)
-    logo = logo.resize((100, 100))  # Ajusta el tamaño del logo según tus necesidades
+    # Ajusta el tamaño del logo según tus necesidades
+    logo = logo.resize((50, 50))
     logo = ImageTk.PhotoImage(logo)
 
-    # Mostrar el logo en la interfaz
-    logo_label = tk.Label(ventana, image=logo)
-    logo_label.pack(pady=10)
+    # Mostrar el logo y el texto en un mismo contenedor
+    contenedor_logo_texto = tk.Frame(ventana)
+    contenedor_logo_texto.pack(pady=10)
+
+    # Mostrar el texto en el contenedor
+    texto_label = tk.Label(
+        contenedor_logo_texto,
+        text="Fundación Chaminade\nInstituto Linares",
+        justify="center",
+        font=fuente_arial)
+    texto_label.pack()
+
+    # Agregar la línea
+    canvas = tk.Canvas(
+        contenedor_logo_texto,
+        width=200,
+        height=2,
+        bd=0,
+        highlightthickness=0,
+        relief='ridge')
+    canvas.create_line(0, 2, 200, 2)
+    canvas.pack()
+
+    # Mostrar el logo en el contenedor
+    logo_label = tk.Label(contenedor_logo_texto, image=logo)
+    logo_label.pack(pady=20)  # Ajusta el valor de pady para aumentar la separación
 
     # Agregar campo de entrada de RUT
-    rut_label = tk.Label(ventana, text="RUT:")
+    rut_label = tk.Label(ventana, text="RUT:", font=fuente_arial)
     rut_label.pack(pady=10)
-    rut_entry = tk.Entry(ventana)
+    rut_entry = tk.Entry(ventana, font=fuente_arial)
     rut_entry.pack()
 
     # Agregar botón para obtener los datos
-    obtener_datos_button = tk.Button(ventana, text="Obtener Datos", command=obtener_datos)
+    obtener_datos_button = tk.Button(
+        ventana,
+        text="Obtener Datos",
+        command=obtener_datos,
+        font=fuente_arial)
     obtener_datos_button.pack(pady=10)
 
     # Ejecutar el bucle de eventos de la interfaz gráfica
